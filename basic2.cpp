e74ce1c256357d10da4e984644eca97ee54d8ee6
tienvuiet <tienxinhzai241@GMAIL.COM>
2024-11-22 21:25:07 +0700

    basic2

---
+++ basic2.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	int dem=23;
+	int n;
+while (n>0){
+	printf("ban hay doan so nguyen do: ");
+	scanf("%d",&n);
+	if(n==dem){
+		break;
+	}
+}
+printf("ban doan dung roiiii");
+return 0;
+}
